1c03c94fe1696d564a08620746cdb97a541bc804
zhouxinyong <zhoudaxia5683@163.com>
2022-01-05 11:05:37 +0800

    fix: fix umd build (#7)

--- packages/components/builder.config.ts
+++ packages/components/builder.config.ts
@@ -4,4 +4,7 @@ export const BuilderConfig: IBuilderConfig = {
   targetLibName: 'ant-design-vue',
   targetLibCjsDir: 'lib',
   targetLibEsDir: 'es',
+  externals: {
+    'ant-design-vue': 'Antd',
+  },
 }
