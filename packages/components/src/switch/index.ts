fb820b482a63c3159b2e1a9fcfa6a52f0f348efc
gehaiyi <1105993686@qq.com>
2022-05-05 20:19:13 +0800

    fix(switch): switch赋值问题修复

--- packages/components/src/switch/index.ts
+++ packages/components/src/switch/index.ts
@@ -1,6 +1,9 @@
 import { connect, mapProps } from '@formily/vue'
 import { Switch as AntdSwitch } from 'ant-design-vue'
 
-export const Switch = connect(AntdSwitch, mapProps({ readOnly: 'read-only' }))
+export const Switch = connect(
+  AntdSwitch,
+  mapProps({ readOnly: 'read-only', value: 'checked' })
+)
 
 export default Switch
